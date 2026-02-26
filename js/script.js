89d9a3c44bf72f9f5b3b9906ed44bd7f147dc0de
ulfsson <e.ulfsson@bellevuecollege.edu>
2026-02-25 17:15:12 -0800

    Laying out initial JS framework.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 let lootList = []; // Set up the loot list array.
+
+class LootItem {
+    constructor(name, value = 0.0, rarity = 1) {
+        this.name = name;
+
+        this.value = Number(Math.abs(value))
+        if (isNaN(this.value)) this.value = 0.0;
+
+        this.rarity = Number(rarity);
+        if (isNaN(this.rarity)) this.rarity = 0;
+    }
+
+    get rarityValue() {
+        // This "rarity" setup is more inspired by MMORPG quality tiers.
+        switch(this.rarity) {
+            case 0: return this.value * 0.75; // Poor
+            case 1: return this.value; // Common
+            case 2: return this.value * 1.25; // Uncommon
+            case 3: return this.value * 1.50; // Rare
+            case 4: return this.value * 1.75; // Epic
+            default: return this.value;
+        }
+    }
+}
+
+for (let i = 0; i < 10; i++) {
+    let randomValue = Number((Math.random() * 10).toFixed(2));
+    let newLoot = new LootItem("Doohickey", randomValue);
+    lootList.push(newLoot);
+}
+
+console.log(lootList);
+
+let totalValue = 0.0;
+for (item of lootList) {
+    console.log(item);
+    totalValue += item.rarityValue;
+}
+
+console.log(totalValue.toFixed(2));
