ba935ba72aa7309d882f94bb83821f23a2942ce8
ulfsson <e.ulfsson@bellevuecollege.edu>
2026-02-27 18:31:12 -0800

    Added methods and new class for loot shuffling.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let lootList = []; // Set up the loot list array.
+let playerCharacterLootDistribution = [];
 
 // Class for creating individual items of loot, and provides a couple of nice features.
 class LootItem {
@@ -6,6 +7,7 @@ class LootItem {
         this.name = name;
 
         // Do a bit of sanity check. In the event the value isn't a number, default to zero.
+        // If it's a negative number, will do the absolute value instead. No negative value allowed!
         this.value = Number(Math.abs(value))
         if (isNaN(this.value)) this.value = 0.0;
 
@@ -26,6 +28,7 @@ class LootItem {
         }
     }
 
+    // Returns the string name of the rarity numerical value on the object.
     get rarityName() {
         switch(this.rarity) {
             case 0: return "Poor";
@@ -39,6 +42,53 @@ class LootItem {
 }
 
 
+// A simple class for storing the loot given to player-characters.
+class playerCharacter {
+    constructor(loot = []) {
+        this.playerNumber = playerNumber;
+        this.loot = loot;
+    }
+
+    // The loot parameter MUST be of type LootItem!
+    giveLoot(loot) {
+        // Only if the object is LootItem type will it be added.
+        if (loot instanceof LootItem) {
+            this.loot.push(loot);
+        }
+    }
+}
+
+
+// Returns a new array consisting of elements shuffled from the given array.
+// This does not touch the array passed to it.
+function shuffleArray(arrayToShuffle) {
+    // Bail out if the array length is 1 or less. No sense in trying to shuffle a size 1 or 0 array.
+    if (arrayToShuffle.length <= 1) return arrayToShuffle;
+
+    let oldArray = arrayToShuffle.slice(); // Duplicate array because we're about to mess with it.
+    let newArray = []; // The new array to push shuffled objects onto;
+    
+    // Bit of a dirty shuffle method, but hey it works.
+    while (oldArray.length > 0) {
+        let randomIndex = Math.floor(Math.random() * oldArray.length);
+        let arrayObject = oldArray[randomIndex]; // Get the object at the random index.
+        newArray.push(arrayObject); // Push that object onto the new array.
+        oldArray.splice(randomIndex, 1); // Removes the object from that index.
+    }
+
+    return newArray;
+}
+
+
+// Shuffles the loot and evenly distributes it to the players.
+// This is purely visual. The user can click the button agan for a new distribution.
+function shuffleLoot() {
+    let shuffled = shuffleArray(lootList);
+    console.log(shuffled);
+}
+
+
+// Creates the table rows for the loot list table and unhides it.
 function updateLootTable() {
     let lootTable = document.getElementById('lootTable');
     
@@ -101,8 +151,7 @@ function addLoot() {
     let newLoot = new LootItem(itemName, itemValue, itemRarity);
     lootList.push(newLoot);
 
-    updateLootTable();
-    
+    updateLootTable();   
 }
 
 
@@ -112,4 +161,22 @@ function removeFromLootTable(index) {
     updateLootTable();
 }
 
+// Set up the event listeners for the buttons.
 document.getElementById('addLootButton').addEventListener('click', addLoot);
+document.getElementById('splitLootButton').addEventListener('click', shuffleLoot);
+
+
+// Some debug testing.
+// let testLoot = new LootItem("Bag of Dust", 1.00, 1);
+// console.log(testLoot);
+
+// let testPlayer = new playerCharacter(1);
+// testPlayer.giveLoot(1);
+
+// console.log(testPlayer);
+
+// testPlayer.giveLoot(testLoot);
+// testPlayer.giveLoot(testLoot);
+// console.log(testPlayer);
+
+// console.log(testPlayer.loot);
